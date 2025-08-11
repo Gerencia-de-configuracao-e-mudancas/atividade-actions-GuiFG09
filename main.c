b071123888a9a6337517805783f8ae0393591c2b
Guilherme Florencio <guiflorencio.08@outlook.com>
2025-08-11 14:07:38 -0300

    adicao do main

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main(){
+    int n;
+    printf("Digite o numero:");
+    scanf("%d", &n );
+    printf("O dobro de %d eh %d", n, n*2);
+    getch();
+}
